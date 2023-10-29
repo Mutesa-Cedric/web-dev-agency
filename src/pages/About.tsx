3f5116f011f5f3a1880204893e951a1897250bf6
Mutesa-Cedric <mutesacedric@gmail.com>
2023-10-29 19:34:31 +0200

    update : init about us + services

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,12 @@
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="h-screen bg-cover  bg-center bg-no-repeat w-full pt-[10vh] flex flex-col  justify-center px-4 sm:px-8 md:px-12 space-y-10 lg:px-16 "
+      style={{
+        backgroundImage: "url(/hero_bg.svg)"
+      }}
+    >
+      
+    </div>
   )
 }
