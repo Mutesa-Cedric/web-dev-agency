459ee0b908afde2816da69c72d37bfcb2b6ac113
Mutesa-Cedric <mutesacedric@gmail.com>
2023-10-29 10:14:14 +0200

    feat : init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full min-h-screen flex items-center justify-center'>
+        <p className='text-orange-600 text-2xl font-medium'>Web Dev Agency</p>
+      </div>
+    </>
+  )
+}
+
+export default App
